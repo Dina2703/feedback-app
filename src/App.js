a4e6bb2c19c0b8d09094b8f9c0dc1b838b488acb
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-08 21:06:27 -0500

    Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ function App() {
     { id: 3, text: "Comment three" },
   ];
 
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="container">
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
-      <div className="comments">
-        <h3>comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment) => (
-            <li key={comment.id}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      {showComments && commentBlock}
     </div>
   );
 }
