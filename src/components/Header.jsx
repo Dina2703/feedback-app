ac6e8a88cbf1034ec424bfeec7d40889477bbf3b
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-10 12:32:06 -0500

    inline style in jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-function Header({text}) {
+function Header({ text, bgColor, textColor }) {
+  //You can put style in a variable
+  const headerStyle = {
+    //backgroundColor: "blue", or
+    backgroundColor: bgColor,
+    //color: "red",
+    color: textColor,
+  };
   return (
-    <header>
+    <header style={headerStyle}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -12,11 +19,15 @@ function Header({text}) {
 
 //setting default props
 Header.defaultProps = {
-  text: "Feedback UI"
-} 
+  text: "Feedback UI",
+  bgColor: "rgba(0, 0, 0, 0.4)",
+  textColor: "#ff6a95",
+};
 
 //Specify the props type, below code checks for props type. isRequired - optional
 Header.propTypes = {
-  text: PropTypes.string.isRequired
-}
+  text: PropTypes.string.isRequired,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
 export default Header;
