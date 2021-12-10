d1f8654625f2906cdeb1700aeffd087a339df2b7
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-10 13:00:06 -0500

    State and UseState Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header text={'Feedback UI'}/>
+      <Header />
       <div className="container">
-        <h1>My App</h1>
+      <FeedbackItem />
       </div>
     </>
   );
