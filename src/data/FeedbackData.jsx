f63d7908124c0fe9d4a16db1ce0c522536bf4677
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-10 14:46:18 -0500

    Managing Global State

---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci error ratione similique ipsum. Veniam atque delectus saepe id ab distinctio ",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci error ratione similique ipsum. Veniam atque delectus saepe id ab distinctio ",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci error ratione similique ipsum. Veniam atque delectus saepe id ab distinctio ",
+  },
+]
+
+export default FeedbackData
